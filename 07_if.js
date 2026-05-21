1cefe48bfec0ba3ac4e17b02d928f93f73ea299d
peanutboi710 <figaro0928@gmail.com>
2026-05-21 14:50:52 +0900

    if practice

---
+++ 07_if.js
@@ -0,0 +1,27 @@
+const age = 25;
+if (age >= 20){
+    console.log("adult");
+}
+
+const score = Math.random() * 100;
+console.log(score);
+
+if (score >= 70) {
+    console.log ("pass");
+}
+else {
+    console.log ("fail");
+}
+
+if (score >= 90) {
+    console.log ("A");
+}
+else if (score >= 80) {
+    console.log ("B");
+}
+else if (score >= 70) {
+    console.log ("C");
+}
+else {
+    console.log ("try again");
+}
